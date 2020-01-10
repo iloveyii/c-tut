87f488867182afdf459bb12320f987de708dbca7
Hazrat Ali <iloveyii@yahoo.com>
2020-01-10 12:01:29 +0100

    Added comments

--- template.c
+++ template.c
@@ -5,6 +5,7 @@ This is a small program to demonstrate all about template in short
 **/
 template <typename T>
 
+// A template is like a dynamic data type that can accept any type of data that you pass at run time
 T greater(T a, T b) {
     return a > b ? a : b;
 }
@@ -14,3 +15,5 @@ int main(void) {
     printf("Greater between two floats a=%0.2f and b=%0.2f is %0.2f \n", 7.1,30.2, greater(7.1,30.2));
     return 0;
 }
+
+// Run : g++ template.c && ./a.out
