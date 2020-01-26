ca490f236b687b767fad0778379ce5a2eef9a866
Hazrat Ali <iloveyii@yahoo.com>
2020-01-26 15:36:54 +0100

    Simplified linked list

--- linked_list.c
+++ linked_list.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-#include <stdlib.h>
 
 /**
 This is a small program to demonstrate a linked list
@@ -10,14 +9,6 @@ struct node {
 };
 typedef struct node node_t; // shortcut for 'struct node'
 
-node_t *create_new_node(int value) {
-    node_t *new_node = (node_t *) malloc(sizeof(node_t));
-    new_node->value = value;
-    new_node->next = NULL;
-    return new_node;
-}
-
-
 void printlist(node_t *head) {
     node_t *temporary = head;
     while(temporary != NULL) {
@@ -27,22 +18,21 @@ void printlist(node_t *head) {
     printf("\n");
 }
 
-node_t *add_new_node(node_t *head, int value) {
-    node_t *tmp = create_new_node(value);
-    tmp->next = head;
-    head = tmp;
-    return head;
-}
-
 int main(void) {
-    node_t n1, n2, n3;
-    node_t *head, *tmp;
-    head = NULL;
 
-    head = add_new_node(head, 10);
-    head = add_new_node(head, 20);
-    head = add_new_node(head, 30);
-    head = add_new_node(head, 40);
+
+    node_t n1, n2, n3;
+    node_t *head;
+
+    n1.value = 10;
+    n2.value = 20;
+    n3.value = 30;
+
+    // Make a list of them by linking
+    head = &n3;
+    n3.next = &n2;
+    n2.next = &n1;
+    n1.next = NULL; // end of linked list
 
     // print list
     printlist(head);
